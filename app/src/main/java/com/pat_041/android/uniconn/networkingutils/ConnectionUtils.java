c332157aec262e42110404a1529e53fbc4ece305
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-10-14 13:33:45 +0530

    added networking utils

---
+++ app/src/main/java/com/pat_041/android/uniconn/networkingutils/ConnectionUtils.java
@@ -0,0 +1,93 @@
+package com.pat_041.android.uniconn.networkingutils;
+
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class ConnectionUtils {
+
+    private ConnectionUtils(){}
+
+    public static URL makeURL(String s){
+        URL url = null;
+        try{
+            url = new  URL(s);
+        }catch (MalformedURLException e){
+            Log.e("ConnectionUtils","Error forming url",e);
+        }
+        return url;
+    }
+
+    public static String makeConnection(String s){
+
+        URL url = makeURL(s);
+        String jsonResponse = null;
+        if(url==null)
+            return jsonResponse;
+
+        try{
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e("ConnectionUtils","Unable to get data from url",e);
+        }
+
+        return jsonResponse;
+
+    }
+
+    public static String makeHttpRequest(URL url)throws IOException{
+
+        String json="";
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try{
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                json = readFromStream(inputStream);
+            }else {
+                Log.e("ConnectionUtils", "Error response code: " + urlConnection.getResponseCode());
+            }
+
+        }catch (IOException e){
+            Log.e("ConnectionUtils","Error retrieving json response",e);
+        }finally {
+            if (urlConnection!=null)
+                urlConnection.disconnect();
+            if(inputStream!=null)
+                inputStream.close();
+        }
+        return json;
+    }
+
+    private static String readFromStream(InputStream inputStream)throws IOException {
+
+        StringBuilder output = new StringBuilder();
+
+        if(inputStream!=null){
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+}
