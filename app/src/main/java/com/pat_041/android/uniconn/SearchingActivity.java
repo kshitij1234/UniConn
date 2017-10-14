e356291b9cc4904511d6e646d35e33fd5d3d7bfb
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-10-14 13:17:19 +0530

    added search view

---
+++ app/src/main/java/com/pat_041/android/uniconn/SearchingActivity.java
@@ -0,0 +1,28 @@
+package com.pat_041.android.uniconn;
+
+import android.app.SearchManager;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+
+public class SearchingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searching);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        // Retrieve the SearchView and plug it into SearchManager
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/pat_041/android/uniconn/networkingutils/ApiCalls.java
@@ -0,0 +1,10 @@
+package com.pat_041.android.uniconn.networkingutils;
+
+
+public class ApiCalls {
+
+    // Define all the different api calls here as public static final constants
+
+
+
+}
