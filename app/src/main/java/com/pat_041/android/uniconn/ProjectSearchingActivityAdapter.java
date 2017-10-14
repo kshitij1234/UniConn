936928a10d9fac6f7844a05660e00cd2f7baea23
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-10-15 02:25:39 +0530

    commiting changes

---
+++ app/src/main/java/com/pat_041/android/uniconn/ProjectSearchingActivityAdapter.java
@@ -0,0 +1,72 @@
+package com.pat_041.android.uniconn;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pat_041.android.uniconn.definitions.SuperObjects;
+
+import java.util.ArrayList;
+
+public class ProjectSearchingActivityAdapter extends RecyclerView.Adapter<SearchingActivityAdapter.ItemViewHolder>{
+
+    private static final String TAG = SearchingActivityAdapter.class.getSimpleName();
+
+    final private ListItemClickListener mOnClickListener;
+
+    private ArrayList<Project> list;
+
+
+    /**
+     * The interface that receives onClick messages.
+     */
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    public ProjectSearchingActivityAdapter(ListItemClickListener listener, ArrayList<Project> l)
+    {
+        list = l;
+        mOnClickListener = listener;
+    }
+
+    @Override
+    public SearchingActivityAdapter.ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(SearchingActivityAdapter.ItemViewHolder holder, int position) {
+        //Log.d(TAG, "#" + position);
+
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public void setList(ArrayList<? extends SuperObjects> l)
+    {
+        list = l;
+        this.notifyDataSetChanged();
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(clickedPosition);
+        }
+
+
+    }
+
+}
