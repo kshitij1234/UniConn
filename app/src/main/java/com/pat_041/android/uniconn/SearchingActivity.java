7d809a2f2992722a50b37a849e96da90a80ab2e5
Abhijit Roy <abhijitroy996@gmail.com>
2017-10-15 00:29:14 +0530

    Gis Based System Almost Online

--- app/src/main/java/com/pat_041/android/uniconn/SearchingActivity.java
+++ app/src/main/java/com/pat_041/android/uniconn/SearchingActivity.java
@@ -13,6 +13,7 @@ import android.content.DialogInterface;
 import android.content.Loader;
 import android.content.Intent;
 
+import android.provider.Settings;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,17 +21,21 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.pat_041.android.uniconn.definitions.College;
 import com.pat_041.android.uniconn.definitions.SuperObjects;
+import com.pat_041.android.uniconn.definitions.UserLocation;
 import com.pat_041.android.uniconn.loaders.CollegeLoader;
 
 import java.util.ArrayList;
@@ -40,6 +45,7 @@ public class SearchingActivity extends AppCompatActivity implements SearchingAct
 
     private int id;
     private ProgressBar mLoadingIndicator;
+    private Button mGisButton;
     private String lsearchQuery;
     private int lsearchType;
     private String lKey;
@@ -63,6 +69,7 @@ public class SearchingActivity extends AppCompatActivity implements SearchingAct
         id = intent.getExtras().getInt("id");
         mErrorView=(TextView)findViewById(R.id.tv_error_message_display);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mGisButton = (Button)findViewById(R.id.detail_gis_button);
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -75,8 +82,22 @@ public class SearchingActivity extends AppCompatActivity implements SearchingAct
 
         recyclerView.setAdapter(mAdapter);
 
-
         collegeCallbacks = new CollegeCallbacks(this,getLoaderManager());
+        mGisButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                LinearLayout gis_layout = (LinearLayout)findViewById(R.id.gis_linear_layout);
+                gis_layout.setVisibility(View.GONE);
+                //showAlert();
+                UserLocation userLoc= new UserLocation(SearchingActivity.this);
+                if(userLoc.isLocationEnabled()){
+                    displayGisBasedSearch(userLoc);
+                }else{
+                    showErrorView();
+                    Log.v("UserLocation Error : ","OnClick Error");
+                }
+            }
+        });
     }
     private void showJsonDataView() {
         // First, make sure the error is invisible
@@ -97,7 +118,6 @@ public class SearchingActivity extends AppCompatActivity implements SearchingAct
         final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
         SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -207,15 +227,11 @@ public class SearchingActivity extends AppCompatActivity implements SearchingAct
     private class CollegeCallbacks implements LoaderManager.LoaderCallbacks<List<College>> {
 
         Context context;
-
         public CollegeCallbacks(Context context, LoaderManager loaderManager) {
             this.context = context;
             loaderManager.initLoader(0, null, this);
 
         }
-
-
-
         @Override
         public Loader<List<College>> onCreateLoader(int id, Bundle args) {
             System.out.println("inside oncreateloader");
@@ -238,6 +254,8 @@ public class SearchingActivity extends AppCompatActivity implements SearchingAct
         @Override
         public void onLoadFinished(Loader<List<College>> loader, List<College> data) {
             System.out.println("inside load finished");
+            LinearLayout gis_layout = (LinearLayout)findViewById(R.id.gis_linear_layout);
+            gis_layout.setVisibility(View.GONE);
             list =(ArrayList<? extends SuperObjects>) data;
             mAdapter.setList(list);
             if(data==null)
@@ -258,4 +276,35 @@ public class SearchingActivity extends AppCompatActivity implements SearchingAct
             mAdapter.setList(new ArrayList<SuperObjects>());
         }
     }
+
+    private void displayGisBasedSearch(UserLocation userLoc){
+        if(userLoc.locate){
+            System.out.println("The value of the address is  :: "+userLoc.userAddress);
+            Log.v("UserLocation : ",""+userLoc.userAddress);
+        }else{
+            showErrorView();
+            Log.v("UserLocation Error : ","Inside displayGISBASED");
+        }
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
 }
---
+++ app/src/main/java/com/pat_041/android/uniconn/definitions/UserLocation.java
@@ -0,0 +1,113 @@
+package com.pat_041.android.uniconn.definitions;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.pat_041.android.uniconn.SearchingActivity;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by Abhijit on 14-10-2017.
+ */
+
+public class UserLocation implements LocationListener {
+    public String userLatitude;
+    public String userLongitude;
+    public String userAddress; //Add a new TextView to your activity_main to display the address
+    public LocationManager locationManager;
+    //public String provider;
+    public boolean locate;
+    public Context mContext;
+    public UserLocation(Context context) {
+        mContext = context;
+        try{
+            locationManager = (LocationManager)mContext.getSystemService(Context.LOCATION_SERVICE);
+            List<String> providers = locationManager.getProviders(true);
+            Location bestLocation = null;
+            for (String provider : providers) {
+                Location l = locationManager.getLastKnownLocation(provider);
+                if (l == null) {
+                    continue;
+                }
+                if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                    // Found best last known location: %s", l);
+                    bestLocation = l;
+                }
+            }
+            //Location location = locationManager.getLastKnownLocation(provider);
+            if (bestLocation != null) {
+                locate = true;
+                System.out.println("Location " + bestLocation + " has been selected.");
+                onLocationChanged(bestLocation);
+            } else {
+                locate = false;
+                userLatitude = "Latitude is not available";
+                userLongitude = "Longitude is not Available";
+                Log.v("UserLocation Error : ","No Location is Available");
+            }
+        }catch(SecurityException e){
+            e.printStackTrace();
+            Log.v("UserLocation Error : ","Constructor Error");
+            locate = false;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //You had this as int. It is advised to have Lat/Loing as double.
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+
+        Geocoder geoCoder = new Geocoder(mContext, Locale.getDefault());
+        try {
+            List<Address> address = geoCoder.getFromLocation(lat, lng, 1);
+            String stateName = address.get(0).getAddressLine(1);
+            Log.v("state name",stateName);
+            userLongitude = String.valueOf(lng);
+            userLatitude = String.valueOf(lat);
+            userAddress = stateName;
+        } catch (IOException e) {
+            // Handle IOException
+            e.printStackTrace();
+            locate = false;
+            Log.v("UserLocation Error : ","IOException");
+        } catch (NullPointerException e) {
+            // Handle NullPointerException
+            e.printStackTrace();
+            locate = false;
+            Log.v("UserLocation Error : ","NUll Pointer Exc");
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(mContext, "Enabled new provider " + provider, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(mContext, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+    public boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+}
