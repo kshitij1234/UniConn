c33d29239aa3211f2d91170cbc070f8ed4be4b16
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-10-14 16:18:35 +0530

    working on recycler view

--- app/src/main/java/com/pat_041/android/uniconn/SearchingActivityAdapter.java
+++ app/src/main/java/com/pat_041/android/uniconn/SearchingActivityAdapter.java
@@ -1,9 +1,18 @@
 package com.pat_041.android.uniconn;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+
+import java.util.ArrayList;
 
 public class SearchingActivityAdapter extends RecyclerView.Adapter<SearchingActivityAdapter.ItemViewHolder>{
 
@@ -15,6 +24,8 @@ public class SearchingActivityAdapter extends RecyclerView.Adapter<SearchingActi
     private static int viewHolderCount;
     private int mNumberItems;
 
+    private ArrayList<SuperObjects> list;
+
     /**
      * The interface that receives onClick messages.
      */
@@ -22,16 +33,27 @@ public class SearchingActivityAdapter extends RecyclerView.Adapter<SearchingActi
         void onListItemClick(int clickedItemIndex);
     }
 
-    public SearchingActivityAdapter(int numberOfItems, ListItemClickListener listener)
+    public SearchingActivityAdapter(int numberOfItems, ListItemClickListener listener, ArrayList<SuperObjects> l)
     {
+        list = l;
         mNumberItems = numberOfItems;
         mOnClickListener = listener;
         viewHolderCount = 0;
     }
 
     @Override
-    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layout = R.layout.search_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately;
+        shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layout, viewGroup, shouldAttachToParentImmediately);
+        ItemViewHolder viewHolder = new ItemViewHolder(view);
+
+        return viewHolder;
+
     }
 
     @Override
@@ -46,12 +68,36 @@ public class SearchingActivityAdapter extends RecyclerView.Adapter<SearchingActi
     }
 
     public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        ImageView mImageView;
+        TextView heading;
+        TextView extra;
+
         public ItemViewHolder(View itemView) {
             super(itemView);
+            mImageView = (ImageView) itemView.findViewById(R.id.list_image_view);
+            heading = (TextView) itemView.findViewById(R.id.list_heading);
+            extra = (TextView) itemView.findViewById(R.id.list_extra);
+            itemView.setOnClickListener(this);
         }
+
+
 
         @Override
         public void onClick(View v) {
+            // create an intent to go to the corresponding item page
+        }
+
+        public void bind(int position) {
+
+            TextDrawable drawable = TextDrawable.builder()
+                    .buildRoundRect(object.getHeading(), Color.RED, 10); // radius in px
+
+            mImageView.setImageDrawable(drawable);
+
+            SuperObject object = list.get(position);
+            heading.setText(object.getHeading());
+            extra.setText(object.getExtra());
 
         }
     }
