d281ddf5236dad2ca35d9da096483f52f76a0a73
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-10-14 14:13:19 +0530

    commiting to pull

---
+++ app/src/main/java/com/pat_041/android/uniconn/SearchingActivityAdapter.java
@@ -0,0 +1,26 @@
+package com.pat_041.android.uniconn;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class SearchingActivityAdapter extends RecyclerView.Adapter<SearchingActivityAdapter.ItemViewHolder>{
+
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ItemViewHolder {
+    }
+}
