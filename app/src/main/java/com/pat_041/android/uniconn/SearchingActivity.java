bb59aa030988c9a8279c4a6e148ea35ee591105f
the-shashwat <shashwattiwariknp@gmail.com>
2017-10-14 19:04:07 -0700

    Added Popup Dialog functionality

--- app/src/main/java/com/pat_041/android/uniconn/SearchingActivity.java
+++ app/src/main/java/com/pat_041/android/uniconn/SearchingActivity.java
@@ -1,13 +1,20 @@
 package com.pat_041.android.uniconn;
 
+import android.app.AlertDialog;
 import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.pat_041.android.uniconn.definitions.SuperObjects;
 
@@ -57,6 +64,41 @@ public class SearchingActivity extends AppCompatActivity implements SearchingAct
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(SearchingActivity.this);
+            // Get the layout inflater
+            LayoutInflater inflater = SearchingActivity.this.getLayoutInflater();
+            final View v = inflater.inflate(R.layout.popup_dialog, null);
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(v)
+                    // Add action buttons
+                    .setPositiveButton("Make Query", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            EditText parameterView=(EditText)v.findViewById(R.id.parameter);
+                            EditText valueView= (EditText)v.findViewById(R.id.value);
+                            String parameter=parameterView.getText().toString();
+                            String value=valueView.getText().toString();
+                            // Add searching over here
+                            System.out.println(parameter);
+                            System.out.println(value);
+                            dialog.cancel();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    }).create().show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private void searchQuery(String query) {
 
